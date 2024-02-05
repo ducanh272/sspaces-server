5956b44e696f5f51a7b14c5a35b963e302815b5b
ducanh272 <ducanh.bui2702@gmail.com>
2024-02-06 00:17:21 +0700

    fix cors third

--- src/main/java/com/hukathon/openspace/config/SecurityConfiguration.java
+++ src/main/java/com/hukathon/openspace/config/SecurityConfiguration.java
@@ -4,9 +4,11 @@ import jakarta.servlet.Filter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -27,7 +29,22 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
+                        // configure CORS for http request from client
+                        .cors(cors -> {
+                            CorsConfigurationSource source = request -> {
+                                CorsConfiguration configuration = new CorsConfiguration();
+                                configuration.addAllowedOrigin("*");
+                                configuration.addAllowedMethod(HttpMethod.PUT);
+                                configuration.addAllowedMethod(HttpMethod.GET);
+                                configuration.addAllowedMethod(HttpMethod.POST);
+                                configuration.addAllowedMethod(HttpMethod.DELETE);
+                                configuration.addAllowedHeader("*");
+                                configuration.applyPermitDefaultValues();
+                                return configuration;
+                            };
+                            cors.configurationSource(source);
+                        })
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/v1/auth/**").permitAll()
                         .anyRequest().authenticated()
